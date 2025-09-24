133f8b7c77b4554dee406fabe79e2a0985272779
Fordsupatat <ford02177@rais.ac.th>
2025-09-24 22:16:27 +0700

    Part 2
    Part 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 import { Metadata } from 'next';
+export const experimental_ppr = true;
 
 export default function RootLayout({
   children,
